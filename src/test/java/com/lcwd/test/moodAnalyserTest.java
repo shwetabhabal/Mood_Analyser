7e04dd33f11e476913dcdc9706ec6cdfefe842a8
Shweta Bhabal <bhabalshweta02@gmail.com>
2024-02-23 17:59:51 +0530

    Null mood

--- src/test/java/com/lcwd/test/moodAnalyserTest.java
+++ src/test/java/com/lcwd/test/moodAnalyserTest.java
@@ -4,20 +4,32 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class moodAnalyserTest {
-    @Test
-    public void analyseMoodTest_noParameter() {
-        moodAnalyser ma = new moodAnalyser();
-        String regex = ma.analyseMood();
-        Assertions.assertEquals("happy", regex);
-    }
-    @Test
-    public void analyseMoodwithParameter() throws MoodAnalyserException{
-        moodAnalyser ma = new moodAnalyser("I am in sad mood");
-        String regex = ma.analyseMood();
-        Assertions.assertEquals("sad",regex);
-    }
+//    @Test
+//    public void analyseMoodTest_noParameter() {
+//        moodAnalyser ma = new moodAnalyser();
+//        String regex = ma.analyseMood();
+//        Assertions.assertEquals("happy", regex);
+//    }
+//    @Test
+//    public void analyseMoodwithParameter() throws MoodAnalyserException{
+//        moodAnalyser ma = new moodAnalyser("I am in sad mood");
+//        String regex = ma.analyseMood();
+//        Assertions.assertEquals("sad",regex);
+//    }
 //    public void analyseMoodTest_happy() {
 //        String regex = moodAnalyser.analyseMood("I am in any Mood");
 //        Assertions.assertEquals("happy", regex);
 //    }
+
+    @Test
+    public void nullSceario(){
+        try{
+            moodAnalyser ma = new moodAnalyser(null);
+            String mood = ma.analyseMood();
+            Assertions.assertEquals("happy", mood);
+        }
+        catch (MoodAnalyserException e){
+
+        }
+    }
 }
